63a6cee95a75c952063071f313a76b8ae2cf4cbd
Macbookpro <macbookpro@Macbookpros-MacBook-Pro.local>
2023-03-23 01:18:51 +0700

    feature add page note found

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,12 @@ Route::group(['prefix' => 'v1/categories', 'namespace' => 'V1'], function () use
     Route::post('/', [CategoryController::class, 'create']);
     Route::put('/', [CategoryController::class, 'update']);
     Route::delete('/{id?}',[CategoryController::class, 'delete'] );
+    Route::any('{any}', function(){
+        return response()->json([
+            'status'    => false,
+            'message'   => 'Page Not Found.',
+        ], 404);
+    })->where('any', '.*');
     
 });
 
