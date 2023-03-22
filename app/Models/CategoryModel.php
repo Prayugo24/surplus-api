2b3c9a3792654749e2b879479a78de7b24c04b5f
Macbookpro <macbookpro@Macbookpros-MacBook-Pro.local>
2023-03-22 23:01:29 +0700

    update param saveData in model

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -1,1 +1,35 @@
-<?phpnamespace App\Models;use Illuminate\Support\Facades\DB;use Illuminate\Database\Eloquent\Model;use App\Category;class CategoryModel extends Model {        public static function saveData($params =[]) {        $name = (isset($params['name']) ? $params['name'] : NULL);        $enable = (isset($params['enable']) ? $params['enable'] : FALSE);                 $post = Category::create([            'name'     => $name,            'enable'   => $enable        ]);                if ($post) {            return [                'success' => true,                'data' => $post            ];        } else {            return [                'success' => false,                'message' => 'Failed to save category'            ];        }    }}
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use App\Category;
+
+class CategoryModel extends Model {
+
+    
+    public static function saveData($params =[]) {
+        $name = (isset($params['name']) ? $params['name'] : NULL);
+        $enable = (isset($params['enable']) && $params['enable'] == 1) ? true : false; 
+
+        
+        $post = Category::create([
+            'name'     => $name,
+            'enable'   => $enable
+        ]);
+        
+
+        if ($post) {
+            return [
+                'success' => true,
+                'data' => $post
+            ];
+        } else {
+            return [
+                'success' => false,
+                'message' => 'Failed to save category'
+            ];
+        }
+
+    }
+}
